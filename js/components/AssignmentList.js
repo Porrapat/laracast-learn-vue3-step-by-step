ccf51d13f5981070b977f7d1d813f6fa41f58971
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-08-18 14:40:01 +0700

    Episode 14

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,46 +1,55 @@
-import Assignment from "./Assignment.js"
+import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: { Assignment, AssignmentTags },
-    template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
 
+    template: `
+        <section v-show="assignments.length" class="w-60">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
+            
             <assignment-tags
                 v-model:currentTag="currentTag"
                 :initial-tags="assignments.map(a => a.tag)" 
             ></assignment-tags>
-
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-                <assignment
+               <assignment 
                     v-for="assignment in filteredAssignments"
-                    :key="assignment.id"
+                    :key="assignment.id" 
                     :assignment="assignment"
                 ></assignment>
             </ul>
-        </section>
+            
+            <slot></slot>
+        </section> 
     `,
 
     props: {
         assignments: Array,
         title: String,
+        canToggle: { type: Boolean, default: false }
     },
 
     data() {
         return {
-            currentTag: 'all'
+            currentTag: 'all',
         };
     },
+
     computed: {
-        filteredAssignments () {
-            if (this.currentTag == 'all')
-            {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
                 return this.assignments;
             }
-            return this.assignments.filter(a => a.tag == this.currentTag);
-        },
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        }
     }
 }
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -3,19 +3,40 @@ import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
     components: { AssignmentList, AssignmentCreate },
+
     template: `
-        <section class="space-y-6">
-            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
-            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
-            <assignment-create @add="add"></assignment-create>
+        <section class="flex gap-8">
+            <assignment-list :assignments="filters.inProgress" title="In Progress">
+                <assignment-create @add="add"></assignment-create> 
+            </assignment-list>
             
+            <div v-show="showCompleted">
+                <assignment-list 
+                    :assignments="filters.completed" 
+                    title="Completed" 
+                    can-toggle
+                    @toggle="showCompleted = !showCompleted"
+                ></assignment-list>
+            </div> 
         </section>
     `,
+
     data() {
         return {
-            assignments: []
+            assignments: [],
+            showCompleted: true
         }
     },
+
+    computed: {
+        filters() {
+            return {
+                inProgress: this.assignments.filter(assignment => ! assignment.complete),
+                completed: this.assignments.filter(assignment => assignment.complete)
+            };
+        }
+    },
+
     created() {
         fetch('http://localhost:3001/assignments')
             .then(response => response.json())
@@ -23,25 +44,12 @@ export default {
                 this.assignments = assignments;
             });
     },
-    computed: {
-        inProgressAssignment() {
-            return this.assignments.filter(a => !a.complete);
-        },
-        completedAssignment() {
-            return this.assignments.filter(a => a.complete);
-        },
-        filters() {
-            return {
-                inProgress: this.assignments.filter(a => !a.complete),
-                completed: this.assignments.filter(a => a.complete)
-            }
-        }
-    },
+
     methods: {
         add(name) {
             this.assignments.push({
                 name: name,
-                complete: false,
+                completed: false,
                 id: this.assignments.length + 1
             });
         }
