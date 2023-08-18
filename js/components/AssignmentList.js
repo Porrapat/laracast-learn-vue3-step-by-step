96e415c50ee9cfd05b391ab66b4fb4be57b8ce6a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-08-18 12:06:10 +0700

    Episode 10 Phase 1

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -3,8 +3,15 @@ export default {
     components: { Assignment },
     template: `
         <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <h2 class="font-bold mb-2">{{ title }}
+                <span>({{ assignments.length }})</span>
+            </h2>
+
+            <div class="flex gap-2">
+                <button class="rounded border px-1 py-px text-xs" v-for="tag in tags">{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
                     v-for="assignment in assignments"
                     :key="assignment.id"
@@ -17,5 +24,11 @@ export default {
     props: {
         assignments: Array,
         title: String,
+    },
+
+    computed: {
+        tags () {
+            return new Set(this.assignments.map(a => a.tag));
+        }
     }
 }
