e5ea5fa1182fa249ab1609f87d5a98dfae51a6ae
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-09-14 12:45:32 +0700

    Episode 20 part 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <script setup>
-  import { ref } from 'vue';
+  import { useStorage } from '@/composables/useStorage';
 
-  let food = ref('');
-
+  let food = useStorage('food', 'tacos');
+  let age = useStorage('age', 20);
 </script>
 
 <template>
   <main>
     <p>What's your favorite food? <input type="text" v-model="food" /></p>
+    <p>What's your age? <input type="text" v-model="age" /></p>
   </main>
 </template>
