cb808bcce602502b7b57b499d7d7205e3a58cff2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-09-14 13:14:17 +0700

    Episode 21

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
-  import { useStorage } from '@/composables/useStorage';
-
-  let food = useStorage('food', 'tacos');
-  let age = useStorage('age', 20);
+  import TabbableTextarea from "@/components/TabbableTextarea.vue";
+  import { ref } from "vue";
+  let comment = ref('initial textarea value');
 </script>
 
 <template>
   <main>
-    <p>What's your favorite food? <input type="text" v-model="food" /></p>
-    <p>What's your age? <input type="text" v-model="age" /></p>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;" />
+    </form>
   </main>
 </template>
