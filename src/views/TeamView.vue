80a41cee0a07206b10df22f86d9e7441fbc0795d
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-09-15 12:02:15 +0700

    Episode 27

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,8 +1,14 @@
 <script setup>
-import team from "@/team.json";
 import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
+import { useTeamStore } from "@/stores/TeamStore";
+let team = useTeamStore();
+team.fill();
+// // Example of calling a store action.
+// setTimeout(() => {
+//   team.grow(25);
+// }, 2000);
 </script>
 
 <template>
