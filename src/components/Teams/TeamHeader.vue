1c4cb7935eb9bd1760522fdc05b893919c653155
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2023-09-15 11:47:26 +0700

    Episode 26 2

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -13,7 +13,7 @@
 
     <div>
       <div class="inline-flex items-center text-3xl relative">
-        <img src="/smiley.png" alt="" class="mr-2">
+        <!-- <img src="" alt="" class="mr-2"> -->
         <h3>{{ team.name }} Team</h3>
         <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">{{ team.spots }}</div>
       </div>
